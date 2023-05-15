2907623c20722c333ff2e91c23e4c1c60fae937f
tyler <ttang7910@bths.edu>
2023-05-15 12:30:28 -0400

    created a key handler class to manage key inputs for WASD made square move depending on what key was pressed

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -4,22 +4,33 @@ import java.awt.*;
 public class GamePanel extends JPanel implements Runnable {
 
     //Screen Settings
-    final int originalTileSize = 16; // 16x16 tile
-    final int scale = 3;
+    final int ORIGINAL_TILE_SIZE = 16; // 16x16 tile
+    final int SCALE = 3;
 
-    final int tileSize = originalTileSize * scale; //48x48 tile
-    final int maxScreenCol = 16;
-    final int maxScreenRow = 12;
-    final int screenWidth = tileSize * maxScreenCol; // 768 pixels
-    final int screenHeight = tileSize * maxScreenRow; // 576 pixels
+    final int TILE_SIZE = ORIGINAL_TILE_SIZE * SCALE; //48x48 tile
+    final int MAX_SCREEN_COL = 16;
+    final int MAX_SCREEN_ROW = 12;
+    final int SCREEN_WIDTH = TILE_SIZE * MAX_SCREEN_COL; // 768 pixels
+    final int SCREEN_HEIGHT = TILE_SIZE * MAX_SCREEN_ROW; // 576 pixels
+
+    final double FPS = 60;
 
     Thread gameThread;
+    KeyHandler keyH = new KeyHandler();
 
+
+    // Set Player's default Position
+
+    int playerX = 100;
+    int playerY = 100;
+    int playerSpeed = 4;
 
     public GamePanel() {
-        setPreferredSize(new Dimension(screenWidth, screenHeight));
+        setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
         setBackground(Color.black);
         setDoubleBuffered(true);
+        addKeyListener(keyH);
+        setFocusable(true);
     }
 
     public void startGameThread() {
@@ -31,6 +42,57 @@ public class GamePanel extends JPanel implements Runnable {
     @Override
     public void run() {
 
-        
+        double drawInterval = 1000000000 / FPS;
+        double delta = 0;
+        long lastTime = System.nanoTime();
+        long currentTime;
+
+        while (gameThread != null) {
+
+            currentTime = System.nanoTime();
+
+            delta += (currentTime - lastTime) / drawInterval;
+
+            lastTime = currentTime;
+
+            if(delta >= 1) {
+                // Updates information such character positions
+                update();
+
+                // Draw: draw the screen with the update information
+                repaint();
+
+                delta--;
+            }
+        }
+
+    }
+
+    public void update() {
+
+        if(keyH.isUpPressed()) {
+            playerY -= playerSpeed;
+
+        } else if (keyH.isDownPressed()) {
+            playerY += playerSpeed;
+
+        } else if (keyH.isLeftPressed()) {
+            playerX -= playerSpeed;
+
+        } else if (keyH.isRightPressed()) {
+            playerX += playerSpeed;
+
+        }
+
+    }
+
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        Graphics2D g2 = (Graphics2D) g;
+
+        g2.setColor(Color.WHITE);
+
+        g2.fillRect(playerX, playerY, TILE_SIZE, TILE_SIZE);
     }
 }
---
+++ src/KeyHandler.java
@@ -0,0 +1,65 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+    private boolean upPressed, downPressed, leftPressed, rightPressed;
+
+    @Override
+    public void keyTyped(KeyEvent e) {
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if(code == KeyEvent.VK_W) {
+            upPressed = true;
+        }
+        if(code == KeyEvent.VK_S) {
+            downPressed = true;
+        }
+        if(code == KeyEvent.VK_A) {
+            leftPressed = true;
+        }
+        if(code == KeyEvent.VK_D) {
+            rightPressed = true;
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+        int code = e.getKeyCode();
+
+        if(code == KeyEvent.VK_W) {
+            upPressed = false;
+        }
+        if(code == KeyEvent.VK_S) {
+            downPressed = false;
+        }
+        if(code == KeyEvent.VK_A) {
+            leftPressed = false;
+        }
+        if(code == KeyEvent.VK_D) {
+            rightPressed = false;
+        }
+
+    }
+
+    public boolean isUpPressed() {
+        return upPressed;
+    }
+
+    public boolean isDownPressed() {
+        return downPressed;
+    }
+
+    public boolean isLeftPressed() {
+        return leftPressed;
+    }
+
+    public boolean isRightPressed() {
+        return rightPressed;
+    }
+}
