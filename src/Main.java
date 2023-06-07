050660e37db303040ed5980dc1b79e9ecf590f62
tyler <ttang7910@bths.edu>
2023-06-06 22:56:12 -0400

    test push

--- src/Main.java
+++ src/Main.java
@@ -3,39 +3,9 @@ import javax.swing.JFrame;
 public class Main {
     public static void main(String[] args) {
 
-//        JFrame window = new JFrame();
-//        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        window.setResizable(false);
-//        window.setTitle("Maze");
-//
-//        GamePanel gamePanel = new GamePanel();
-//        window.add(gamePanel);
-//
-//        window.pack();
-//
-//        window.setLocationRelativeTo(null);
-//        window.setVisible(true);
-//
-//
-//        JFrame tilePick = new JFrame();
-//        tilePick.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//        tilePick.setResizable(false);
-//        tilePick.setTitle("Tile Picker");
-//
-//        TilePickerPanel drawingPanel = gamePanel.getDrawingPanel();
-//        tilePick.add(drawingPanel);
-//        tilePick.pack();
-//        tilePick.setLocation(40, 175);
-//        tilePick.setVisible(true);
-//
-//
-//        gamePanel.startGameThread();
-//        drawingPanel.startGameThread();
+        //Test
 
         MainMenuGUI mainMenu = new MainMenuGUI();
-
-
-
 
     }
 }
